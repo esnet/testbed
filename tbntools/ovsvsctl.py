a9420ddc850d239ebb21ebe1662677a460b063d2
lomaxfrog <lomaxfrog@yahoo.com>
2016-08-10 17:52:50 -0700

    tbntools now implements ovs-vsctl show

--- tbntools/ovsvsctl.py
+++ tbntools/ovsvsctl.py
@@ -3,10 +3,10 @@ import subprocess
 from tbntools import flaskApp
 from flask import request
 
-@flaskApp.route('/ovs/vs/show/<value>')
+@flaskApp.route('/ovs/vs/show')
 def show(value=None):
     global flaskApp
-    output = subprocess.check_output(['ovs-vsctl','show',value])
+    output = subprocess.check_output(['ovs-vsctl','show'])
     response = flaskApp.make_response(output)
     response.headers["content-type"] = "text/plain"
     return response
